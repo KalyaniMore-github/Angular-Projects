c0ad4e4e36ecbe4ac3f279468de0841e2be78567
KalyaniMore-github <141003934+KalyaniMore-github@users.noreply.github.com>
2023-09-08 16:16:42 +0530

    project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { ProductlistComponent } from './productlist/productlist.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    EmployeeComponent,
+    ProductlistComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+  title="product list";
+  companylogo="https://static.toiimg.com/thumb/msid-67586673,width-800,height-600,resizemode-75,imgsize-3918697,pt-32,y_pad-40/67586673.jpg";
+
+  constructor(){}
+  ngOnInit() {
+    
+  }
+}
